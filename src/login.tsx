a4a2c124b84ff21fb503cb0c60ec00c9dc8c05c6
Shashank Gavale <shashankmgavale2004@gmail.com>
2025-01-19 18:48:46 +0530

    New Project #1

---
+++ src/login.tsx
@@ -0,0 +1,20 @@
+export default function login() {
+  return (
+    <>
+      <div className="flex p-8 bg-slate-800 m-10 h-full">
+        {/* This is Main Div */}
+        <div className="flex-1 bg-black h-full text-white m-6">
+          HEYYY.
+          {/* This is For Fisrt Half */}
+
+          </div>
+
+        <div className="flex-1 bg-fuchsia-600 h-full m-6 p-4 text-white">
+          {/* For Second Half  */}
+          <h3 className="text-3xl">Sign In</h3>
+
+        </div>
+      </div>
+    </>
+  );
+}
